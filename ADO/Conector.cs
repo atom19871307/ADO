b23867e4f327500342be4e331f80c79883129d94
Atom Mayilyan <mayilyan.atom@mail.ru>
2026-04-01 18:57:53 +0300

    Fixed Null  Reference Exception, added App.config and dynamic column sizing

---
+++ ADO/Conector.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace PV_522_ADO
+{
+	class Connector
+	{
+		SqlConnection connection;
+		public Connector(string connection_string)
+		{
+			connection = new SqlConnection(connection_string);
+			Console.WriteLine(connection.ConnectionString);
+		}
+		public void Select(string cmd)
+		{
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+
+			//Определяем максимальный размер данных в каждом поле таблицы:
+			int[] string_sizes = new int[reader.FieldCount];    //Этот массив хранит максимальные размеры строк для каждого поля.
+			int interval = 15;
+			//Вычисляем максимальные размеры строк:
+			for (int i = 0; i < reader.FieldCount; i++)
+				if (reader.GetName(i).ToString().Length > string_sizes[i])
+					string_sizes[i] = reader.GetName(i).ToString().Length + interval;
+			while (reader.Read())
+			{
+				for (int i = 0; i < reader.FieldCount; i++)
+				{
+					if (reader[i].ToString().Length > string_sizes[i])
+						string_sizes[i] = reader[i].ToString().Length + interval;
+				}
+			}
+			reader.Close();
+			//Заново выполняем запрос, и выводим результаты запроса:
+			reader = command.ExecuteReader();
+			for (int i = 0; i < reader.FieldCount; i++)
+			{
+				Console.Write($"{reader.GetName(i).PadRight(string_sizes[i])}");
+				//if (reader.GetName(i).ToString().Length > string_sizes[i]) string_sizes[i] = reader.GetName(i).ToString().Length + 1;
+			}
+			Console.WriteLine();
+			for (int i = 0; i < string_sizes.Sum(); i++) Console.Write("-"); Console.WriteLine();
+			while (reader.Read())
+			{
+				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
+				for (int i = 0; i < reader.FieldCount; i++)
+					Console.Write(reader[i].ToString().PadRight(string_sizes[i]));
+				Console.WriteLine();
+			}
+			reader.Close();
+			connection.Close();
+		}
+		public void Select(string fields, string tables, string condition = "")
+		{
+			string cmd = $"SELECT {fields} FROM {tables} ";
+			if (condition != "") cmd += $" WHERE {condition}";
+			Select(cmd);
+		}
+		public object Scalar(string cmd)
+		{
+			object value = null;
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			value = command.ExecuteScalar();
+			connection.Close();
+			return value;
+		}
+	}
+}
--- ADO/Program.cs
+++ ADO/Program.cs
@@ -1,107 +1,44 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using System.Data.SqlClient;
+using System.Configuration;
 
 //ADO.NET
 //ADO - ActiveX Data Objects
-// .NET
-//System.Date.SqlClient
+//.NET
+//System.Data
+//System.Data.SqlClient
 
-namespace ADO
+namespace PV_522_ADO
 {
 	class Program
 	{
-		private const string movies_PV_522_connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_522;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
-
-		private static T_SQL movies_PV_522 = new T_SQL(new SqlConnection(movies_PV_522_connection_string));
 		static void Main(string[] args)
 		{
-			Console.WriteLine(movies_PV_522_connection_string);
+			//string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_522;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			string connection_string = ConfigurationManager.ConnectionStrings["Movies"].ConnectionString;
+			Connector connector = new Connector(connection_string);
 
+			connector.Select("SELECT * FROM Directors");
+			connector.Select("title,first_name,last_name", "Movies,Directors", "director=director_id");
 
-			string cmd = "SELECT * FROM Directors";
-			movies_PV_522.Select(cmd);
-			Console.WriteLine($"Колличество записей: {movies_PV_522.Scalar("SELECT COUNT(*) FROM Directors")}");
-			movies_PV_522.Select
-				(
-				"SELECT " + "title , date = release_date , first_name , last_name " +"FROM Movies , Directors " +"WHERE director_id = director"
-				);
+			//Console.WriteLine(connection_string);
+			//connection = new SqlConnection(connection_string);
+
+			//string cmd = "SELECT last_name,first_name FROM Directors";
+			//Select(cmd);
+
+			//Console.WriteLine($"Количество записей: {Scalar("SELECT COUNT(*) FROM Directors")}");
+			//Select("title, release_date, last_name, first_name", "Movies,Directors", "director=director_id");
+			/*Select("SELECT title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id");*/
+
 		}
 
-	}
-	public class T_SQL
-	{
-		private SqlConnection connection;
-
-		public T_SQL(SqlConnection _connection) { connection = _connection; }
-		public T_SQL(ref SqlConnection _connection) { connection = _connection; }
-		public void Select(string cmd, int interval = 11)
-		{
-			SqlCommand command = new SqlCommand(cmd, connection);
-			connection.Open();
-			SqlDataReader reader = command.ExecuteReader();
-			string[][] output = new string[1][];
-			output[0] = new string[reader.FieldCount];
-
-			for (int i = 0; i < reader.FieldCount; i++)
-				output[0][i] = $"[ {reader.GetName(i)} ]";
-			/*
-			for (int i = 0; i < reader.FieldCount; i++)
-				Console.Write($"[ {reader.GetName(i)} ]\t");
-			Console.WriteLine();
-			*/
-			while (reader.Read())
-			{
-				output = output.Append(new string[reader.FieldCount]).ToArray();
-				for (int i = 0; i < reader.FieldCount; i++)
-					output[output.Length - 1][i] = reader[i].ToString();
-				/*
-				string output_line = "";
-				for(int i = 0; i < reader.FieldCount; ++i) 
-					output_line += reader[i].ToString() + '\t';
-				Console.WriteLine(output_line);
-				*/
-				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
-				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader.FieldCount}");
-			}
-			reader.Close();
-			connection.Close();
-
-			for (int i = 0; i < output[0].Length; ++i)
-			{
-				int max_size_str = 0;
-				for (int j = 0; j < output.Length; ++j)
-					if (max_size_str < output[j][i].Length) max_size_str = output[j][i].Length;
-
-				for (int j = 0; j < output.Length; ++j)
-					output[j][i] += new string(' ', max_size_str - output[j][i].Length + interval);
-			}
-			for (int i = 0; i < output[0].Length; ++i)
-				Console.Write(output[0][i]);
-			Console.WriteLine($"\n{new string('-', output[0].Sum(str => str.Length))}");
-
-			for (int i = 1; i < output.Length; ++i)
-			{
-				foreach (string line in output[i])
-					Console.Write(line);
-				Console.WriteLine();
-			}
-		}
-		//static void Select(string fields, stringo tables, string conditon = "")
-			//string cmd = $"SELECT {fields} FROM {tables}";
-		public object Scalar(string cmd)
-		{
-			object value = null;
-			SqlCommand command = new SqlCommand(cmd, connection);
-			connection.Open();
-			value = command.ExecuteScalar();
-			connection.Close();
-			return value;
-		}
 
 	}
 }
