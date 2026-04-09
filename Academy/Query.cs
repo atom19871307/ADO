7bf80ea8503a84356d84b1f57959201bb5503b65
Atom Mayilyan <mayilyan.atom@mail.ru>
2026-04-09 20:36:52 +0300

    Custom queries on tabs

---
+++ Academy/Query.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Query
+	{
+		public string Fields { get; set;}
+		public string Tables{ get; set; }
+		public string Condition { get; set; }
+		public Query (string  fislds, string tables, string condition = "")
+		{
+			this.Fields = fislds;
+			this.Tables = tables;
+			this.Condition = condition;
+		}
+		public override string ToString()
+		{
+			string cmd = $"SELECT {Fields} FROM {Tables}";
+			if (Condition != "") cmd += $" WHERE {Condition}";
+			return cmd;
+		}
+
+	}
+}
