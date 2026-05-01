94810d81836fab26e5bc2f59f2a2f600230fbafa
Atom Mayilyan <mayilyan.atom@mail.ru>
2026-05-01 10:04:34 +0300

    AqademyWPF

---
+++ AcedemyWPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Configuration;
+using DBtools;
+using System.Runtime.InteropServices;
+
+namespace AcedemyWPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		Connector connector;
+		DataGrid[] tables;
+		public MainWindow()
+		{
+			AllocConsole();
+			InitializeComponent();
+			connector = new Connector(ConfigurationManager.ConnectionStrings["PV_522_Import"].ConnectionString);
+			tables = new DataGrid[] { gdvStudents, gdvGrups, gdvDirections, gdvDisciplines, gdvTeachers };
+			tabControl.SelectedIndex = 0;
+			tabControl_SelectionChanged(tabControl, null);
+		}
+		[DllImport("kernel32.dll")]
+		public static extern bool AllocConsole();
+
+		private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			Console.WriteLine(tabControl.SelectedIndex);
+			tables[tabControl.SelectedIndex].ItemsSource = 
+				connector.Load($"SELECT * FROM {(tabControl.SelectedItem as TabItem).Header.ToString()}").DefaultView;
+			statusBarCount.Text = $"Количество записей: {tables[tabControl.SelectedIndex].Items.Count - 1}";
+        }
+    }
+}
